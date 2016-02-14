07aae26d49cd1a8364dbb3bdf34f2d83ab53d4f7
Austin Liu <arl96@cornell.edu>
2016-02-14 00:47:06 -0500

    totally legit commits yo

---
+++ app/src/main/java/treehacks/tinderplusplus/LookingFor.java
@@ -0,0 +1,13 @@
+package treehacks.tinderplusplus;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LookingFor extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_looking_for);
+    }
+}
