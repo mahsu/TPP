5640143e3b1bec30bfd5c4937dcea4c863273a66
Rajat Bhageria <rajat1996@gmail.com>
2016-02-14 01:22:18 -0800

    add login with phone

--- app/src/main/java/treehacks/tinderplusplus/LoginActivity.java
+++ app/src/main/java/treehacks/tinderplusplus/LoginActivity.java
@@ -29,6 +29,10 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.digits.sdk.android.AuthCallback;
+import com.digits.sdk.android.DigitsAuthButton;
+import com.digits.sdk.android.DigitsException;
+import com.digits.sdk.android.DigitsSession;
 import com.twitter.sdk.android.Twitter;
 import com.twitter.sdk.android.core.*;
 import com.twitter.sdk.android.core.TwitterAuthConfig;
@@ -369,8 +373,9 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
         Intent i = new Intent(this, Personal.class);
         startActivity(i);
     }
+
     private TwitterLoginButton loginButton;
-    public void twitterLogin(){
+    private void twitterLogin(){
         loginButton = (TwitterLoginButton) findViewById(R.id.twitter_login_button);
         loginButton.setCallback(new Callback<TwitterSession>() {
             @Override
@@ -396,6 +401,24 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
         // Make sure that the loginButton hears the result from any
         // Activity that it triggered.
         loginButton.onActivityResult(requestCode, resultCode, data);
+    }
+
+    private void phoneLogin(){
+        DigitsAuthButton digitsButton = (DigitsAuthButton) findViewById(R.id.auth_button);
+        digitsButton.setCallback(new AuthCallback() {
+            @Override
+            public void success(DigitsSession session, String phoneNumber) {
+                // TODO: associate the session userID with your user model
+                Toast.makeText(getApplicationContext(), "Authentication successful for "
+                        + phoneNumber, Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void failure(DigitsException exception) {
+                Log.d("Digits", "Sign in with Digits failure", exception);
+            }
+        });
+
     }
 
 
