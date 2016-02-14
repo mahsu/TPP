1f5298c24de014c7b9afccb536883d63ad7547f4
Rajat Bhageria <rajat1996@gmail.com>
2016-02-14 10:42:13 -0800

    added the aysnc http requests and added sharedpreferences

--- app/src/main/java/treehacks/tinderplusplus/Matching.java
+++ app/src/main/java/treehacks/tinderplusplus/Matching.java
@@ -49,7 +49,7 @@ public class Matching extends AppCompatActivity {
         for (String key: settings.getAll().keySet()){
             params.put(key,settings.getString(key +"", ""));
             System.out.println(key);
-            System.out.println(client.get("http://tinderplusplus.mybluemix.net/data/getAns/1",null));
+            //System.out.println(client.get("http://tinderplusplus.mybluemix.net/data/selectAns/1"));
         }
     }
 }
